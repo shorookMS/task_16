1c0c188a76beac4ea9cf21b5fc3c0a9717e69db0
shorookMS <shorook.m.saleh@gmail.com>
2018-10-16 16:40:26 +0300

    Task submission

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.generics import (
     RetrieveAPIView,
     RetrieveUpdateAPIView,
     DestroyAPIView,
+    CreateAPIView,
 )
 from .serializers import (
     RestaurantListSerializer,
@@ -24,7 +25,11 @@ class RestaurantDetailView(RetrieveAPIView):
 
 
 # Complete Me
-class RestaurantCreateView():
+class RestaurantCreateView(CreateAPIView):
+    serializer_class = RestaurantCreateUpdateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class RestaurantUpdateView(RetrieveUpdateAPIView):
